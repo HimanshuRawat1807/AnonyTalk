8b703f408bf47335cfefa192b897a4a3f7674dd6
Himanshu Rawat <himurawat1807@gmail.com>
2026-04-09 19:17:02 +0530

    dotenv setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port =  Process.env.PORT;
+const port =  process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
