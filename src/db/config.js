98be498e97bcd83b27553b561859a67635d2cbc0
Himanshu Rawat <himurawat1807@gmail.com>
2026-04-10 19:56:53 +0530

    database conntion

---
+++ src/db/config.js
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+
+const connectionDB = async() => {
+
+    if (!process.env.MONGO_URL) {
+        console.error(`Database URL not defined in .env `);
+        process.exit(1);
+    }    
+
+    try {
+        const connection = await mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
+        console.log(`MongoDB connected : ${connection.connection.host}`);
+        
+    } catch (error) {
+        console.error("Databse connection Error : ", error.message);
+        process.exit(1) 
+    }
+}
+
+export default connectionDB;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import "dotenv/config"; 
+import dotenv from "dotenv";
 import express from "express"
+import connectionDB from "./db/config.js";
+
+dotenv.config({
+  path:"./.env"
+});
+
+
 const app = express()
+const PORT =  process.env.PORT ||  3000;
 
-const port =  process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const  startServer = () => {
+
+  connectionDB()
+  .then(()=>{
+    app.listen(PORT || 3000, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
 })
+  .catch((error)=>{
+        console.error("MongoDB XOnnection Failed : ",error);
+        
+  })
+};
+
+startServer();
 
 
-app.get("/", (req,res)=>{
-    
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
 
 
